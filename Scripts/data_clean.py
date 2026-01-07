59eb326cdbbee12c4959fc3a8e5565323cd850ef
Viola Joel <tamviola17@gmail.com>
2026-01-07 11:50:06 +0000

    python script update

--- Scripts/data_clean.py
+++ Scripts/data_clean.py
@@ -1,74 +1,53 @@
 import pandas as pd
 
 
-## Book Data
-
-# Import Book data into a Pandas dataframe
-book_dataframe = pd.read_csv("./Sample data/03_Library Systembook.csv")
-
-# Remove columns with missing Id or Book name
-book_df = book_dataframe.dropna(subset=["Id","Books"])
-
-# Convert Book Returned column to date
-book_df.loc[:, "Book Returned"] = pd.to_datetime(book_df["Book Returned"])
-# book_df["Book Returned"].dtype
-
-# Remove double quotes around the Book checkout dates
-book_df.loc[:, "Book checkout"] = book_df["Book checkout"].str.strip('"')
-
-# Update Book checkout date for Book checkout is 32/05/2023
-book_df.loc[book_df["Book checkout"] == "32/05/2023", "Book checkout"] = "31/05/2023"
-# book_df[book_df["Book checkout"] == "32/05/2023"]
-
-# Convert Book chceckout column to date
-book_df["Book checkout"] = pd.to_datetime(book_df["Book checkout"], format="%d/%m/%Y")
-# book_df["Book checkout"].dtype
-# book_df
-
-# Convert Book Returned column to date
-book_df["Book Returned"] = pd.to_datetime(book_df["Book Returned"], format="%d/%m/%Y")
-# book_df["Book Returned"].dtype
-
-# Remove time and keep date only
-book_df.loc[:, "Book checkout"] = pd.to_datetime(book_df["Book checkout"]).dt.date
-book_df.loc[:, "Book Returned"] = pd.to_datetime(book_df["Book Returned"]).dt.date
-
-# Find duplicate records
-# book_df[book_df.duplicated(subset=["Books"], keep=False)]
-
-# Drop duplicate records
-book_df = book_df.drop_duplicates(subset=["Books"])
-
-# Convert float id values to int
-# book_df.loc[:, ["Id", "Customer ID"]] = book_df[["Id", "Customer ID"]].astype("Int64")    # Didn't work
-
-book_df["Customer ID"] = book_df[["Customer ID"]].astype("Int64")
-book_df["Id"] = book_df[["Id"]].astype("Int64")
+### Book Data
 
 # Add load duration column to book dataframe
 def loan_duration(df):
     return df['Book Returned'] - df['Book checkout']
-book_df['Loan duration'] = loan_duration(df=book_df)
+
+
+def clean_book_data(input_file_path, output_file_path):
+    # Import data into a Pandas dataframe
+    book_df = pd.read_csv(input_file_path)
+    # Drop duplicate records
+    book_df = book_df.drop_duplicates()
+    # Remove rows with null Id or Book name
+    book_df = book_df.dropna(subset=["Id","Books"])
+    # Remove leading and trailing spaces from Book name
+    book_df["Books"] = book_df["Books"].str.strip()
+    # Remove double quotes around the Book checkout dates
+    book_df.loc[:, "Book checkout"] = book_df["Book checkout"].str.strip('"')
+    # Update Book checkout date for Book checkout is 32/05/2023
+    book_df.loc[book_df["Book checkout"] == "32/05/2023", "Book checkout"] = "31/05/2023"   # Generalise this
+    # Convert dates
+    book_df["Book checkout"] = pd.to_datetime(book_df["Book checkout"], format="%d/%m/%Y")
+    book_df["Book Returned"] = pd.to_datetime(book_df["Book Returned"], format="%d/%m/%Y")
+    # Convert float id values to int
+    book_df["Customer ID"] = book_df[["Customer ID"]].astype("Int64")
+    book_df["Id"] = book_df[["Id"]].astype("Int64")
+    # Enrich dataframe
+    book_df['Loan duration'] = loan_duration(df=book_df)
+    # Write cleaned data into csv file
+    book_df.to_csv(output_file_path, index=False)
+    print("Book file cleaned.")
+
+clean_book_data(input_file_path="./Sample data/03_Library Systembook.csv", output_file_path="./Cleaned data/03_Library Systembook.csv")
+
 
 
 
 ### Customer Data
-'''
-# Import Customers data into a Pandas dataframe
-customers_dataframe = pd.read_csv("./Sample data/03_Library Systemcustomers.csv")
-customers_dataframe
-
-# Drop null values
-customers_df = customers_dataframe.dropna(subset=["Customer ID"])
-
-# Convert float Customer id values to int
-customers_df["Customer ID"] = customers_df[["Customer ID"]].astype("Int64")
-'''
 
 def clean_customer_data(input_file_path, output_file_path):
+    # Import data into a Pandas dataframe
     cust_df = pd.read_csv(input_file_path)
+    # Drop null values
     cust_df = cust_df.dropna(subset=["Customer ID"])
+    # Convert float Customer id values to int
     cust_df["Customer ID"] = cust_df[["Customer ID"]].astype("Int64")
+    # Write cleaned data into csv file
     cust_df.to_csv(output_file_path, index=False)
     print("Customer file cleaned.")
 
